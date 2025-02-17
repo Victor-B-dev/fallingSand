e99aef6f86b1c0cc983123b7224ae41032f229c3
Victor <ban.victor728@gmail.com>
2025-02-17 16:05:29 -0500

    added makeGrid function that will house the particles (randomly 5), also created setup which will create the canvas (p5 function) on which to house the grid

--- sketch.js
+++ sketch.js
@@ -1,2 +1,26 @@
+const makeGrid = (cols, rows) =>{
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+    // Fill the array with 0s
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = 0;
+    }
+  }
+  return arr;
+}
+
+let grid = []; // the grid that will be drawn on top of the canvas
+let particle = 5; // the size of each cell
+
+const setup = () =>{
+  createCanvas(800, 600) // this is a p5 that will be the window -- doing this without p5 would be an event listener on a designated div to be the canvas
+  cols = width / particle;
+  rows = height / particle;
+  grid = makeGrid(cols, rows);
+}
+
+ 
+
 
 
