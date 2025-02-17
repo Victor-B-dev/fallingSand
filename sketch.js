7e626ae5cba9c73ecc46594337808aea64d7269a
Victor <ban.victor728@gmail.com>
2025-02-17 12:49:52 -0500

    wiped sketch.js to restart without types, keeping it simple

--- sketch.js
+++ sketch.js
@@ -1,25 +1,2 @@
-class Grid {
-  initialize(width, height) {
-    this.width = width;
-    this.height = height;
-    this.grid = new Array(width * height).fill(0);
-  }
 
-  clear() {
-    this.grid = new Array(this.width * this.height).fill(0);
-  }
 
-  set(x, y, color) {
-    this.grid[y * this.width + x] = color;
-  }
-  
-  swap(a, b) {
-    const temp = this.grid[a];
-    this.grid[a] = this.grid[b];
-    this.grid[b] = temp;
-  }
-  
-  isEmpty(index) {
-    return this.grid[index] === 0;
-  }
-}
