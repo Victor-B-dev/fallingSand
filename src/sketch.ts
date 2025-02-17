217ea08dd6faf754e850b0ec3584ebae4016ef09
Victor <ban.victor728@gmail.com>
2025-02-17 11:22:42 -0500

    added class properties to Grid & constructor function, also added typing to the initialize

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,7 +1,29 @@
 class Grid {
-  initialize(width, height) {
+  private width: number;
+  private height: number;
+  private grid: number[][];
+
+  constructor() {
+    this.width = 0;
+    this.height = 0;
+    this.grid = [];
+  }
+
+  initialize(width: number, height: number) {
     this.width = width;
     this.height = height;
     this.grid = new Array(width * height).fill(0);
   }
+
+
+
+
+
+
+
+
+
+
+
+
 }
